d34bf295b0d0da8b51b28a5b956d5ebd3575ee1c
heeeione <heewon410@naver.com>
2024-11-17 11:07:29 +0900

    style: Change test scroll and main contents

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -7,11 +7,11 @@ const DefaultLayout = () => {
     <div className="flex flex-col min-h-svh w-full sm:max-w-[800px] mx-auto ">
       <Header />
       <main className="flex-grow pt-16 pb-20">
+        <Outlet />
         <div className="h-[2000px] bg-gray-100">
           <h1 className="text-3xl text-center py-10">스크롤 테스트</h1>
           <p className="text-center">테스트</p>
         </div>
-        <Outlet />
       </main>
       <FooterNav />
     </div>
