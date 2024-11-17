904ccdbaa05db62747eda00cf0a6367eb709fa97
heeeione <heewon410@naver.com>
2024-11-17 10:30:34 +0900

    style: Add opacity header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,10 +1,13 @@
+import MainLogo from '../../assets/common/MainLogo.svg';
+
 const Header = () => {
   return (
-    <header className="bg-white border-b border-gray-300">
+    <header className="transition-all duration-300 fixed top-0 left-0 w-full z-10 bg-transparent border-b border-gray-300">
       <div className="container mx-auto flex justify-center items-center h-16">
-        <img alt="로고" className="h-8 w-auto" />
+        <img src={MainLogo} alt="로고" className="h-8 w-auto" />
       </div>
     </header>
   );
 };
+
 export default Header;
