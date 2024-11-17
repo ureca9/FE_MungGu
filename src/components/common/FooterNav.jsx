9760b32f083a67b44ee3a7ad6f6447282efa63eb
heeeione <heewon410@naver.com>
2024-11-17 10:31:35 +0900

    style: Fit width to footer main

--- src/components/common/FooterNav.jsx
+++ src/components/common/FooterNav.jsx
@@ -12,28 +12,30 @@ const navItems = [
 
 const FooterNav = () => {
   return (
-    <nav className="flex justify-around items-center bg-white border-t border-gray-300 p-4">
-      {navItems.map((item) => (
-        <NavLink
-          key={item.path}
-          to={item.path}
-          className={({ isActive }) =>
-            `flex flex-col items-center text-xs mt-1 ${
-              isActive ? 'text-black' : 'text-gray-500'
-            } hover:text-blue-500`
-          }
-        >
-          {item.icon ? (
-            <>
-              <item.icon size={24} />
-              <span className="text-xs mt-1">{item.label}</span>
-            </>
-          ) : (
-            <img src={FooterLogo} alt="메인 로고" className="h-14 w-auto" />
-          )}
-        </NavLink>
-      ))}
-    </nav>
+    <footer className="w-full bg-white border-t border-gray-300 fixed bottom-0 left-0 z-10">
+      <nav className="max-w-[800px] mx-auto flex justify-between items-center p-3">
+        {navItems.map((item) => (
+          <NavLink
+            key={item.path}
+            to={item.path}
+            className={({ isActive }) =>
+              `flex flex-col items-center text-xs mt-1 ${
+                isActive ? 'text-black' : 'text-gray-500'
+              } hover:text-blue-500`
+            }
+          >
+            {item.icon ? (
+              <>
+                <item.icon size={24} />
+                <span className="text-xs mt-1">{item.label}</span>
+              </>
+            ) : (
+              <img src={FooterLogo} alt="메인 로고" className="h-14 w-auto" />
+            )}
+          </NavLink>
+        ))}
+      </nav>
+    </footer>
   );
 };
 
