93488c5b3fb0c750188d157b93015935ff3d00f2
EunSeok <dldmstjr114@naver.com>
2024-11-21 02:07:58 +0900

    feat:PetPage/UserPage생성

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,8 +3,10 @@ import DefaultLayout from '../layout/DefaultLayout.jsx';
 import Main from '../pages/Main.jsx';
 import Like from '../pages/Like.jsx';
 import Map from '../pages/Map.jsx';
-import MyPage from '../pages/MyPage.jsx';
+import MyPage from '../pages/UserPage/MyPage.jsx';
 import Search from '../pages/Search.jsx';
+import UserUpdatePage from '../pages/UserPage/UserUpdatePage.jsx';
+import PetUpdatePage from '../pages/PetPage/PetUpdatePage.jsx';
 
 const routes = [
   {
@@ -26,6 +28,14 @@ const routes = [
   {
     path: '/search',
     element: <Search />,
+  },
+  {
+    path: '/user-update',
+    element: <UserUpdatePage />,
+  },
+  {
+    path: '/pet-update',
+    element: <PetUpdatePage />,
   },
 ];
 
