1531b91d22395e69e7ae7e387c3055ccbc49bb7c
채희원 <68272931+heeeione@users.noreply.github.com>
2024-11-27 14:17:11 +0900

    refactor: Reset Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,5 @@
-import LoadingSpinner from '../components/common/LoadingSpinner.jsx';
-
 const Main = () => {
-  return (
-    <>
-      <div className="text-blue-500">메인 페이지</div>
-      <LoadingSpinner />
-    </>
-  );
+  return <div className="text-blue-500">메인 페이지</div>;
 };
 
 export default Main;
