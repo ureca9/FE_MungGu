684d9b6aefa96ea17b66c90846162ffd30e04652
heeeione <heewon410@naver.com>
2024-11-27 14:01:35 +0900

    style: Add loading spinner

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,12 @@
+import LoadingSpinner from '../components/common/LoadingSpinner.jsx';
+
 const Main = () => {
-  return <div className="text-blue-500">메인 페이지</div>;
+  return (
+    <>
+      <div className="text-blue-500">메인 페이지</div>
+      <LoadingSpinner />
+    </>
+  );
 };
 
 export default Main;
