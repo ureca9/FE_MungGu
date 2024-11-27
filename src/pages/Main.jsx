777d68d73471f653fc44ea09e499dd5eae5f8a9a
heeeione <heewon410@naver.com>
2024-11-27 17:54:57 +0900

    refactor: Reset Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,20 +1,5 @@
-import useLoadingStore from '../stores/common/useLoadingStore.js';
-import LoadingSpinner from '../components/common/LoadingSpinner.jsx';
-
 const Main = () => {
-  const { isLoading, setIsLoading } = useLoadingStore();
-
-  const toggleLoading = () => {
-    setIsLoading(!isLoading);
-  };
-
-  return (
-    <>
-      <div className="text-blue-500">메인 페이지</div>
-      <button onClick={toggleLoading}>loading</button>
-      {isLoading ? <LoadingSpinner /> : <span>X</span>}
-    </>
-  );
+  return <div className="text-blue-500">메인 페이지</div>;
 };
 
 export default Main;
