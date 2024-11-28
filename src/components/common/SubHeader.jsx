3f902cceacfd0c9e99bbefa03ab165f9bd1d4fab
ckdwns1221 <ckdwns2181@gmail.com>
2024-11-28 16:22:39 +0900

    feat: 라우터 변수처리 후 SubHeader가 필요한 각페이지에 맞게 구현

--- src/components/common/SubHeader.jsx
+++ src/components/common/SubHeader.jsx
@@ -1,12 +1,45 @@
 import { IoIosArrowBack } from 'react-icons/io';
+import { useLocation, useNavigate } from 'react-router-dom';
+import ROUTER_PATHS from '../../utils/RouterPath';
 
 const SubHeader = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const pageTitles = {
+    [ROUTER_PATHS.MAP]: '강아지도',
+    [ROUTER_PATHS.MY_PAGE]: '마이페이지',
+    [ROUTER_PATHS.LOGIN]: '로그인',
+    [ROUTER_PATHS.LIVE_REVIEW]: '실시간 리뷰',
+    [ROUTER_PATHS.USER_REGISTER]: '회원 등록',
+    [ROUTER_PATHS.DOG_REGISTER]: '강아지 등록',
+    [ROUTER_PATHS.PREFERENCE_PLANT]: '선호 시설 등록',
+    [ROUTER_PATHS.PREFERENCE_REGION]: '선호 지역 등록',
+    [ROUTER_PATHS.REGION_NAME]: '지역명',
+    [ROUTER_PATHS.PLACE_NAME]: '장소명',
+    [ROUTER_PATHS.ALL_REVIEW]: '리뷰 전체보기',
+    [ROUTER_PATHS.DOG_UPDATE]: '강아지 수정',
+    [ROUTER_PATHS.DOG_ADD]: '강아지 추가',
+    [ROUTER_PATHS.USER_UPDATE]: '회원 정보 수정',
+    [ROUTER_PATHS.MY_REVIEW]: '내가 쓴 후기',
+    [ROUTER_PATHS.MY_REVIEW_MODIFY]: '후기 수정',
+    [ROUTER_PATHS.RECENT_VIEW_LIST]: '최근 본 목록',
+    [ROUTER_PATHS.RESERVATION]: '예약',
+    [ROUTER_PATHS.RESERVATION_COMPLETE]: '예약완료',
+    [ROUTER_PATHS.RESERVATION_LIST]: '예약 내역',
+    [ROUTER_PATHS.MEONGSENG_NEACUT]: '멍생네컷',
+  };
+
+  const pageTitle = pageTitles[location.pathname] || '';
+
   return (
-    <header className="fixed top-0 left-0 z-10 w-[768px] border-b border-gray-300 backdrop-blur-md">
-      <div className="container flex items-center justify-between h-16 mx-auto">
-        <IoIosArrowBack />
-        <div>페이지제목</div>
-        <div></div>
+    <header className="fixed top-0 z-10 w-[768px] border-b border-gray-300 backdrop-blur-md">
+      <div className="container flex items-center justify-between h-16 px-4 mx-auto">
+        <button onClick={() => navigate(-1)} className="text-xl">
+          <IoIosArrowBack />
+        </button>
+        <div className="text-lg font-bold">{pageTitle}</div>
+        <div className="w-5"></div>
       </div>
     </header>
   );
--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -2,14 +2,38 @@ import { Outlet, useLocation } from 'react-router-dom';
 import Header from '../components/common/Header.jsx';
 import SubHeader from '../components/common/SubHeader.jsx';
 import FooterNav from '../components/common/FooterNav.jsx';
+import ROUTER_PATHS from '../utils/RouterPath.js';
 
 const DefaultLayout = () => {
   const location = useLocation();
-  const isHome = location.pathname === '/';
+
+  const isSubHeaderPath = [
+    ROUTER_PATHS.MAP,
+    ROUTER_PATHS.MY_PAGE,
+    ROUTER_PATHS.LOGIN,
+    ROUTER_PATHS.LIVE_REVIEW,
+    ROUTER_PATHS.USER_REGISTER,
+    ROUTER_PATHS.DOG_REGISTER,
+    ROUTER_PATHS.PREFERENCE_PLANT,
+    ROUTER_PATHS.PREFERENCE_REGION,
+    ROUTER_PATHS.REGION_NAME,
+    ROUTER_PATHS.PLACE_NAME,
+    ROUTER_PATHS.ALL_REVIEW,
+    ROUTER_PATHS.DOG_UPDATE,
+    ROUTER_PATHS.DOG_ADD,
+    ROUTER_PATHS.USER_UPDATE,
+    ROUTER_PATHS.MY_REVIEW,
+    ROUTER_PATHS.MY_REVIEW_MODIFY,
+    ROUTER_PATHS.RECENT_VIEW_LIST,
+    ROUTER_PATHS.RESERVATION,
+    ROUTER_PATHS.RESERVATION_COMPLETE,
+    ROUTER_PATHS.RESERVATION_LIST,
+    ROUTER_PATHS.MEONGSENG_NEACUT,
+  ].includes(location.pathname);
 
   return (
     <div className="flex flex-col min-h-svh w-full sm:max-w-[768px] mx-auto shadow-2xl">
-      {isHome ? <Header /> : <SubHeader />}
+      {isSubHeaderPath ? <SubHeader /> : <Header />}
       <main className="flex-grow pt-16 pb-20">
         <Outlet />
         <div className="h-[2000px]">
