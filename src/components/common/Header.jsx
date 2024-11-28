c425adbd0627b74f5c630486de39ae54350a4659
ckdwns1221 <ckdwns2181@gmail.com>
2024-11-28 13:56:17 +0900

    feat:header에 로그인페이지 이동 추가

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
   return (
     <header className="fixed top-0 left-0 z-10 w-full border-b border-gray-300 backdrop-blur-md">
-      <a className="container flex items-center justify-center h-16 mx-auto LogoFont text-[#3288FF] no-underline text-3xl">
+      <Link
+        to="/login"
+        className="container flex items-center justify-center h-16 mx-auto LogoFont text-[#3288FF] no-underline text-3xl"
+      >
         멍티비티
-      </a>
+      </Link>
     </header>
   );
 };
