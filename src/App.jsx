806c5d1c5a4e9d6ad71e2601f5b41d011c3cddd4
ckdwns1221 <ckdwns2181@gmail.com>
2024-11-29 14:47:52 +0900

    fix: 로그인 상태 유지

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './router/router.jsx';
-
+import { useEffect } from 'react';
+import useLoginStore from './stores/login.js';
+import LOCAL_STORAGE_KEYS from './utils/LocalStorageKey.js';
 const App = () => {
-  return <RouterProvider router={router} />;
+  const { setLogin } = useLoginStore();
+  useEffect(() => {
+    const memberId = localStorage.getItem(LOCAL_STORAGE_KEYS.MEMBER_ID);
+    if (memberId) {
+      setLogin();
+    }
+  }, [setLogin]);
+  return (
+    <>
+      <RouterProvider router={router} />;
+    </>
+  );
 };
 
 export default App;
--- src/stores/login.js
+++ src/stores/login.js
@@ -1,7 +1,8 @@
 import { create } from 'zustand';
+import LOCAL_STORAGE_KEYS from '../utils/LocalStorageKey';
 
 const useLoginStore = create((set) => ({
-  isLoggedIn: false,
+  isLoggedIn: !!localStorage.getItem(LOCAL_STORAGE_KEYS.MEMBER_ID),
   setLogin: () => set({ isLoggedIn: true }),
   setLogout: () => set({ isLoggedIn: false }),
 }));
