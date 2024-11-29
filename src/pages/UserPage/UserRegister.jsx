64567fa67146cab12cb90ddc090263dc49a88774
ckdwns1221 <ckdwns2181@gmail.com>
2024-11-29 16:23:20 +0900

    fix: input Storybook 수정

--- src/pages/UserPage/UserRegister.jsx
+++ src/pages/UserPage/UserRegister.jsx
@@ -3,7 +3,11 @@ import { BasicInput } from '../../stories/Input/BasicInput';
 const UserRegister = () => {
   return (
     <div className="w-2/3 mx-auto mt-10">
-      <BasicInput label="이름" id="name" placeholder="이름을 입력하세요" />
+      <div>
+        <div>프로필 이미지</div>
+        input
+      </div>
+      <BasicInput label="이메일" id="name" placeholder="이름을 입력하세요" />
     </div>
   );
 };
--- src/stories/Input/BasicInput.jsx
+++ src/stories/Input/BasicInput.jsx
@@ -1,20 +1,42 @@
 import PropTypes from 'prop-types';
 
 /* 
-  <BasicInput label="이름" id="name" placeholder="이름을 입력하세요"/> 
+  <BasicInput 
+    label="이름" 
+    id="name" 
+    type="text" 
+    placeholder="이름을 입력하세요" 
+    style="gray" 
+  /> 
 */
 
-export const BasicInput = ({ label, placeholder, id, ...props }) => {
+export const BasicInput = ({
+  label,
+  placeholder = '여기에 적어주세요.',
+  id,
+  type = 'text',
+  style = 'gray',
+  ...props
+}) => {
+  const styleClass = {
+    gray: 'bg-gray-100 border-gray-300 text-gray-700 text-sm',
+    blue: 'bg-[#3288FF] text-white text-base',
+  };
+
+  const selectedClass = styleClass[style] || styleClass.gray;
+
   return (
     <div className="flex flex-col">
-      <label htmlFor={id} className="mb-1 text-[15px] font-bold">
-        {label}
-      </label>
+      {type !== 'submit' && (
+        <label htmlFor={id} className="mb-1 text-[15px] font-bold">
+          {label}
+        </label>
+      )}
       <input
-        type="text"
+        type={type}
         id={id}
         placeholder={placeholder}
-        className={`inline-block cursor-pointer leading-none rounded-[8px] w-full bg-white border border-[#8a8a8a] font-light px-3 py-3 text-sm`}
+        className={`inline-block leading-none rounded-[8px] w-full border font-light px-3 py-3 ${selectedClass}`}
         {...props}
       />
     </div>
@@ -22,7 +44,9 @@ export const BasicInput = ({ label, placeholder, id, ...props }) => {
 };
 
 BasicInput.propTypes = {
-  label: PropTypes.string,
-  placeholder: PropTypes.string,
-  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired, // label은 필수
+  placeholder: PropTypes.string, // 기본값으로 빈 문자열
+  id: PropTypes.string.isRequired, // id는 필수
+  type: PropTypes.oneOf(['radio', 'email', 'text', 'file', 'submit']), // 허용된 타입
+  style: PropTypes.oneOf(['gray', 'blue']), // gray와 blue 디자인 옵션
 };
