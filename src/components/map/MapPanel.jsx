7fa423f0787035d7985ec977603f969e8511a4bd
heeeione <heewon410@naver.com>
2024-11-29 13:24:11 +0900

    feat: Add bottom panel

--- src/components/map/MapPanel.jsx
+++ src/components/map/MapPanel.jsx
@@ -1,20 +1,32 @@
 import PropTypes from 'prop-types';
 
-const MapPanel = ({ isPanelExpanded, setIsPanelExpanded }) => {
+const MapPanel = ({ panelState, setPanelState }) => {
+  const panelHeightClass = {
+    collapsed: 'h-20',
+    expanded: 'h-2/5',
+    maximized: 'h-[calc(100vh-15rem)]',
+  };
+
   return (
     <div
-      className={`absolute bottom-0 left-0 right-0 z-20 bg-white rounded-t-lg shadow-lg transition-all duration-300 ${
-        isPanelExpanded ? 'h-3/5' : 'h-20'
-      }`}
+      className={`absolute bottom-0 left-0 right-0 z-20 bg-white rounded-t-lg shadow-lg transition-all duration-300 ${panelHeightClass[panelState]}`}
     >
       <div
         className="flex items-center justify-center p-4 cursor-pointer"
-        onClick={() => setIsPanelExpanded(!isPanelExpanded)}
+        onClick={() => {
+          if (panelState === 'collapsed') {
+            setPanelState('expanded');
+          } else if (panelState === 'expanded') {
+            setPanelState('maximized');
+          } else {
+            setPanelState('collapsed');
+          }
+        }}
       >
         <div className="w-12 h-1 bg-gray-400 rounded-full"></div>
       </div>
 
-      {isPanelExpanded && (
+      {panelState !== 'collapsed' && (
         <div className="p-4 overflow-y-auto">
           <p>찜목록</p>
         </div>
@@ -24,8 +36,9 @@ const MapPanel = ({ isPanelExpanded, setIsPanelExpanded }) => {
 };
 
 MapPanel.propTypes = {
-  isPanelExpanded: PropTypes.bool.isRequired,
-  setIsPanelExpanded: PropTypes.func.isRequired,
+  panelState: PropTypes.oneOf(['collapsed', 'expanded', 'maximized'])
+    .isRequired,
+  setPanelState: PropTypes.func.isRequired,
 };
 
 export default MapPanel;
--- src/pages/Map/Map.jsx
+++ src/pages/Map/Map.jsx
@@ -4,16 +4,13 @@ import MapContainer from '../../components/map/MapContainer';
 import MapPanel from '../../components/map/MapPanel';
 
 const Map = () => {
-  const [isPanelExpanded, setIsPanelExpanded] = useState(false);
+  const [panelState, setPanelState] = useState('expanded');
 
   return (
     <div className="relative w-full h-[calc(100vh-8rem)]">
       <MapSearch />
       <MapContainer />
-      <MapPanel
-        isPanelExpanded={isPanelExpanded}
-        setIsPanelExpanded={setIsPanelExpanded}
-      />
+      <MapPanel panelState={panelState} setPanelState={setPanelState} />
     </div>
   );
 };
