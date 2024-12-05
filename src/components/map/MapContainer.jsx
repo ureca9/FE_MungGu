efad6c676c8a7d62165b96ed4c4508f29825c2ba
heeeione <heewon410@naver.com>
2024-12-05 16:53:07 +0900

    feat: 찜한 장소 마커 추가

--- src/components/map/MapContainer.jsx
+++ src/components/map/MapContainer.jsx
@@ -1,11 +1,12 @@
 import { useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Swal from 'sweetalert2';
+import { getMarkers } from '../../api/map/map.js';
 
 const MapContainer = ({ onMapLoaded }) => {
   const mapContainer = useRef(null);
 
-  const initMap = (latitude, longitude) => {
+  const initMap = async (latitude, longitude) => {
     const map = new window.kakao.maps.Map(mapContainer.current, {
       center: new window.kakao.maps.LatLng(latitude, longitude),
       level: 3,
@@ -15,6 +16,25 @@ const MapContainer = ({ onMapLoaded }) => {
       position: new window.kakao.maps.LatLng(latitude, longitude),
     });
     marker.setMap(map);
+
+    const places = await getMarkers();
+    places.forEach((place) => {
+      const marker = new window.kakao.maps.Marker({
+        position: new window.kakao.maps.LatLng(
+          Number(place.latitude),
+          Number(place.longitude),
+        ),
+        map,
+      });
+
+      const infoWindow = new window.kakao.maps.InfoWindow({
+        content: `<div style="padding:5px;">${place.name}</div>`,
+      });
+
+      window.kakao.maps.event.addListener(marker, 'click', () => {
+        infoWindow.open(map, marker);
+      });
+    });
   };
 
   const waitForKakaoMaps = (retries = 10) => {
